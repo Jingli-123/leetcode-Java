71f4ee5af55c31b01c2a78050bf3b16bda8d7334
Jingli-123 <jingli88669@gmail.com>
2025-11-23 13:43:33 +1100

    day2 array part 02

--- day2.java
+++ day2.java
@@ -1,21 +1,22 @@
 /**209. Minimum Size Subarray Sum
  * Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+ * Approach: sliding windows. set i as start point, j as end point
  */
 
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int i = 0;
         int min = Integer.MAX_VALUE;
-        int sum = nums[0];
-        for(int j = 1; j < nums.length; j++){
-            if(nums[i] == target){
-                return 1;
-            }
+        int length = 0;
+        int sum = 0;
+        for(int j = 0; j < nums.length; j++){
             sum += nums[j];
-            if(sum > target){
+            length++;
+            while(sum >= target){
+                min = Math.min(length, min);
+                sum -= nums[i];
                 i++;
-            }else if(sum == target && min > (j - i)){
-                min = j - i;
+                length--;
             }
         }
         if(min == Integer.MAX_VALUE) return 0;
