ced4ae400cd021c303e49cb0477f0f93cada0f49
Jingli-123 <s3980386@student.rmit.edu.au>
2025-12-02 17:52:54 +1100

    day9

---
+++ day9.java
@@ -0,0 +1,139 @@
+/**232. Implement Queue using Stacks
+ * https://leetcode.com/problems/implement-queue-using-stacks/description/
+ * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+ */
+class MyQueue {
+    private Stack<Integer> stackIn;
+    private Stack<Integer> stackOut;
+    public MyQueue() {
+        stackIn = new Stack<>();
+        stackOut = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stackIn.push(x);
+    }
+    
+    public int pop() {
+        pushToStackIn();
+        return stackOut.pop();
+    }
+    
+    public int peek() {
+        pushToStackIn();
+        return stackOut.peek();
+    }
+    
+    public boolean empty() {
+        return stackIn.isEmpty() && stackOut.isEmpty();
+    }
+    public void pushToStackIn(){
+        if(stackOut.isEmpty()){
+            while(!stackIn.isEmpty()){
+                stackOut.push(stackIn.pop());
+            }
+        }
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+/**225. Implement Stack using Queues
+ * https://leetcode.com/problems/implement-stack-using-queues/description/
+ * Implement a last-in-first-out (LIFO) stack using only two queues. The implemented 
+ * stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+ */
+class MyStack {
+    private Queue<Integer> que;
+
+    public MyStack() {
+        que = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        que.offer(x);
+        int s = que.size();
+        //reverse queue
+        for(int i = 0; i < s - 1; i++){
+            que.offer(que.poll());
+        }
+    }
+    
+    public int pop() {
+        return que.poll();
+    }
+    
+    public int top() {
+        return que.peek();
+    }
+    
+    public boolean empty() {
+        return que.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+/**20. Valid Parentheses
+ * https://leetcode.com/problems/valid-parentheses/
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', 
+ * determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+
+ */
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> deque = new Stack<>();
+        // Deque<Character> deque = new LinkedList<>(); both works
+        char ch;
+        for(int i = 0; i < s.length(); i++){
+            ch = s.charAt(i);
+            if(ch == '('){
+                deque.push(')');
+            }else if(ch == '{'){
+                deque.push('}');
+            }else if(ch == '['){
+                deque.push(']');
+            }else if(deque.isEmpty() || deque.peek() != ch){
+                return false;
+            }else{
+                deque.pop();
+            }
+        }
+        return deque.isEmpty();
+    }
+}
