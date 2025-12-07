e3b016044ea89b07977f48a74871c4d159b5a72a
Jingli-123 <s3980386@student.rmit.edu.au>
2025-12-08 09:39:30 +1100

    day13

---
+++ day13.java
@@ -0,0 +1,81 @@
+/**102. Binary Tree Level Order Traversal
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+ * Given the root of a binary tree, return the level order traversal of its nodes' 
+ * values. (i.e., from left to right, level by level).
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        int level = 0;
+        level(root, level);
+        return result;
+    }
+    public void level(TreeNode root, int level){
+        if(root == null) return;
+        level++;
+        if(result.size() < level){
+            List<Integer> item = new ArrayList<Integer>();
+            result.add(item);
+        }
+        result.get(level - 1).add(root.val);
+        level(root.left, level);
+        level(root.right, level);
+    }
+}
+
+/**107. Binary Tree Level Order Traversal II
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ * Given the root of a binary tree, return the bottom-up level order traversal of 
+ * its nodes' values. (i.e., from left to right, level by level from leaf to root).
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> result = new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        int level = 0;
+        level(root, level);
+        Collections.reverse(result);
+        return result;
+    }
+    public void level(TreeNode root, int level){
+        if(root == null)return;
+        level++;
+        
+        if(result.size()<level){
+            List<Integer> item = new ArrayList<Integer>();
+            result.add(item);
+        }
+        level(root.left, level);
+        level(root.right, level);
+        result.get(level - 1).add(root.val);
+    }
+}
