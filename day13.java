97f79b91d566f255c821a60f4ee95e15a6cb894f
Jingli-123 <s3980386@student.rmit.edu.au>
2025-12-08 17:58:01 +1100

    day13

--- day13.java
+++ day13.java
@@ -79,3 +79,50 @@ class Solution {
         result.get(level - 1).add(root.val);
     }
 }
+
+/**199. Binary Tree Right Side View
+ * https://leetcode.com/problems/binary-tree-right-side-view/description/
+ * Given the root of a binary tree, imagine yourself standing on the right 
+ * side of it, return the values of the nodes you can see ordered from top to bottom.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        Deque<TreeNode> que = new LinkedList<>();
+        if(root == null)return result;
+        que.offerLast(root);
+        while(!que.isEmpty()){
+            int level = que.size();
+            for(int i = 0; i < level; i++){
+                TreeNode poll = que.pollFirst();
+                if(poll.left != null){
+                    que.addLast(poll.left);
+                }
+                if(poll.right != null){
+                    que.addLast(poll.right);
+                }
+                if(i == level -1){
+                    result.add(poll.val);
+                }
+            }
+        }
+        return result;
+        
+    }
+}
